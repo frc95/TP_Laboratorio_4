7e50f93582883fc4f736539d0b03699c322613c0
frc95 <frc95zz@gmail.com>
2020-09-10 10:34:17 -0300

    Amarilla Godoy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RutaUnoComponent } from './components/ruta-uno/ruta-uno.component';
+import { RutaDosComponent } from './components/ruta-dos/ruta-dos.component';
+import { HomeComponent } from './components/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RutaUnoComponent,
+    RutaDosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
